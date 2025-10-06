0ca9421357248284400537aefe542d8ba039cd4d
Ishwor Subedi <ishworsubedi13@gmail.com>
2025-10-06 17:26:14 +0300

    t5 of module 3 done.

--- JavaScript/Module 3/t5/5.js
+++ JavaScript/Module 3/t5/5.js
@@ -93,3 +93,30 @@ const picArray = [
 ];
 
 // add your code here
+const section = document.getElementById('pictures');
+
+for (let i = 0; i < picArray.length; i++) {
+  const article = document.createElement('article');
+  article.classList.add('card');
+
+  const h2 = document.createElement('h2');
+  h2.textContent = picArray[i].title;
+  article.appendChild(h2);
+
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  img.src = picArray[i].image.medium;
+  img.alt = picArray[i].title;
+  figure.appendChild(img);
+
+  const figcaption = document.createElement('figcaption');
+  figcaption.textContent = picArray[i].caption;
+  figure.appendChild(figcaption);
+  article.appendChild(figure);
+
+  const p = document.createElement('p');
+  p.textContent = picArray[i].description;
+  article.appendChild(p);
+
+  section.appendChild(article);
+}
